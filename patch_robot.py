6047e782f276ecdd990af75d9349e074d5164b42
charles.droszcz <charles.droszcz@gmail.com>
2021-10-25 16:41:16 +0200

    patch_robot.py: Add of encoding=utf-8

--- patch_robot.py
+++ patch_robot.py
@@ -4,11 +4,11 @@
 import sys
 
 for html_file in sys.argv[1:]:
-    html = open(html_file, 'r')
+    html = open(html_file, 'r', encoding='utf-8')
     contents = html.readlines()
     html.close()
     contents.insert(contents.index('<head>\n') + 1,
                     '<meta name="robots" content="noindex, nofollow"/>\n')
-    html = open(html_file, 'w')
+    html = open(html_file, 'w', encoding='utf-8')
     html.write(''.join(contents))
     html.close()
